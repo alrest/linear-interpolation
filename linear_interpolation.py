7583a32d4df580222e68b5f9e41dad86fa5dd8fa
alrest <112730926+alrest@users.noreply.github.com>
2022-09-09 15:24:32 -0500

    final

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -11,27 +11,25 @@ from math import *
 # Assignment:   LAB: Linear Interpolation
 # Date:         09-09-2022
 
-# PART 1
 
-# coordinates
 x1 = 10
 y1 = 2026
 x2 = 55
 y2 = 23026
 
-# calculates the slope
 m = (y2-y1)/(x2-x1)
-
-# equation that will solve for distance from houston given time
 
 t = 25
 dist = m * (t-x1) + y1
+print("Part 1:")
 print("For t =", t, "minutes, the position p =", dist, "kilometers")
 
 # PART 2
+print()
 
 t = 300
 r = 6745
 c = 2 * pi * r
 dist = (m * (t-x1) + y1) % c
+print("Part 2")
 print("For t =", t, "minutes, the position p =", dist, "kilometers")
