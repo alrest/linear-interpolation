8b12f4f6035640d2450a9c6423d5e65466bc125e
alrest <112730926+alrest@users.noreply.github.com>
2022-09-09 11:02:05 -0500

    Part 1

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -1,3 +1,4 @@
+from math import *
 # By submitting this assignment, I agree to the following:
 #   "Aggies do not lie, cheat, or steal, or tolerate those who do."
 #   "I have not given or received any unauthorized aid on this assignment."
@@ -8,4 +9,17 @@
 # Assignment:   THE ASSIGNMENT NUMBER (e.g. Lab 1b-2)
 # Date:         DAY MONTH YEAR
 
-# I'm typing this sentence to see if you can pull
+# coordinates
+x1 = 10
+y1 = 2026
+x2 = 55
+y2 = 23026
+
+# calculates the slope
+m = (y2-y1)/(x2-x1)
+
+# equation that will solve for distance from houston given time
+
+t = 25
+dist = m * (t-x1) + y1
+print("The distance of the ISS from Houston at time", t, "is", dist)
